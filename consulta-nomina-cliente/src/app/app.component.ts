e37d3999b513c2d299f62df30d5dd4a78a7da1af
David Andres Ramirez Vanegas <david.ramirez@ibm.com>
2019-07-12 11:43:38 -0500

    final version 1

--- consulta-nomina-cliente/src/app/app.component.ts
+++ consulta-nomina-cliente/src/app/app.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
-import { Person } from "./person"
-import {PayrollService } from "./payroll.service"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-//  title = 'consulta-nomina-cliente';
-	
-	payrollService;	
-	personel;
-	
-	constructor (payrollService: PayrollService){
-		this.payrollService = payrollService;
-	}
-	
-  search() {
-	this.payrollService.getPayroll().subscribe((data) => this.personel = data)
-//	this.personel = [new Person("Queso",1), new Person("Galletas",2)];
-	console.log("Actualizado")
-};
+
+	nombre;
+	id;
+	sueldo;
+	input;
+
+	search(valor: String) {
+		if (valor === "") {
+			return;
+		}
+
+		fetch("http://localhost:8080/darUnEmpleadoAlTunel/?name=" + valor, {
+			method: 'GET',
+			mode: 'cors'
+		}).then(response => response.json())
+			.then(myJson => {
+				console.log(myJson);
+				this.nombre = myJson["nombre"];
+				this.id = myJson["id"];
+				this.sueldo = myJson["salario"];
+			});
+	};
 }
